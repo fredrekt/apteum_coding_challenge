7c5799642de73c6e4f6e812e568dec158ac149cf
fredrekt <fredrickjohng6@gmail.com>
2024-01-31 17:56:39 +0800

    init with dummy data

--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Homepage.scss';
 import { Typography } from 'antd';
 import DefaultLayout from '../../layouts/default/DefaultLayout';
+import DetailsDrawer from '../../drawers/DetailsDrawer/DetailsDrawer';
+import { Property } from '../../types/property.types';
 
 const Homepage: React.FC = () => {
+	const [selectedProperty, setSelectedProperty] = useState<Property | null>(null);
+	const [openDrawer, setOpenDrawer] = useState<boolean>(false);
+
+	useEffect(() => {
+		setSelectedProperty({
+			property_id: 1525674,
+			council: 'CARDINIA',
+			council_property_number: '2188100300',
+			full_address: '8 CLOVERLEIGH AVENUE EMERALD 3782',
+			latitude: -37.9373447811622,
+			lga_code: 311,
+			longitude: 145.449895817713,
+			postcode: '3782'
+		});
+	}, []);
+
 	return (
 		<DefaultLayout>
-			<Typography.Title>Homepage</Typography.Title>
+			<Typography.Title onClick={() => setOpenDrawer(true)}>Homepage</Typography.Title>
+			{selectedProperty && (
+				<DetailsDrawer
+					key={selectedProperty.property_id}
+					opened={openDrawer}
+					onCancel={() => setOpenDrawer(!openDrawer)}
+					selectedData={selectedProperty}
+				/>
+			)}
 		</DefaultLayout>
 	);
 };
