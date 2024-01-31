1496e0598c24ddc18012e628097f0e9ff1a31980
fredrekt <fredrickjohng6@gmail.com>
2024-01-31 17:02:51 +0800

    added property types

---
+++ src/types/property.types.ts
@@ -0,0 +1,10 @@
+export interface Property {
+	id: number;
+	council: string;
+	council_property_number: string;
+	full_address: string;
+	latitude: number;
+	lga_code: number;
+	longitude: number;
+	postcode: string;
+}
