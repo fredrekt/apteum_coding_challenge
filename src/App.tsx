5f39ddfd257011b0fd6580086d52f02e3fd8935a
fredrekt <fredrickjohng6@gmail.com>
2024-01-31 17:42:22 +0800

    setup init pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Homepage from './pages/home/Homepage';
+import Missingpage from './pages/missing/Missingpage';
 
 const App: React.FC = () => {
-	return <div></div>;
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" index element={<Homepage />} />
+				<Route path="*" element={<Missingpage />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
