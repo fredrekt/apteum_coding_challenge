8fbdf03960bb3e8e40eb611e97db0b543c31d227
fredrekt <fredrickjohng6@gmail.com>
2024-01-31 17:42:14 +0800

    added config for theme customization

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
+import { theme } from './utils/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
 	<React.StrictMode>
-		<App />
+		<ConfigProvider theme={theme}>
+			<App />
+		</ConfigProvider>
 	</React.StrictMode>
 );
 
---
+++ src/utils/theme.ts
@@ -0,0 +1,30 @@
+/* 
+
+Customize ant design theme
+link: https://ant.design/theme-editor
+
+*/
+
+export const theme = {
+	token: {
+		fontFamily: `'Montserrat', sans-serif`,
+		colorPrimary: '#3bb778'
+	},
+	components: {
+		Button: {
+			borderRadius: 0,
+			borderRadiusLG: 0
+		},
+		Input: {
+			borderRadius: 0
+		},
+		Select: {
+			borderRadius: 0,
+			borderRadiusLG: 0
+		},
+		Card: {
+			borderRadius: 0,
+			borderRadiusOuter: 0
+		}
+	}
+};
